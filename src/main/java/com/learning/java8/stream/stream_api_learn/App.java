2d31337e04887985913bc977ab84118d0165b901
Akhil Kurup <akhil.kurup@inseinc.com>
2023-03-23 11:58:34 +0530

    initial commit

---
+++ src/main/java/com/learning/java8/stream/stream_api_learn/App.java
@@ -0,0 +1,13 @@
+package com.learning.java8.stream.stream_api_learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
